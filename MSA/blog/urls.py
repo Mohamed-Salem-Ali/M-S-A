bb0b9521ff9852791afe29eae3b0368134d6d15e
Mohamed-Salem-Ali <65522205+Mohamed-Salem-Ali@users.noreply.github.com>
2024-07-16 17:20:18 +0300

    .

--- MSA/blog/urls.py
+++ MSA/blog/urls.py
@@ -3,9 +3,13 @@ from . import views
 urlpatterns = [
     
     path("",views.starting_page, name="starting-page"),
+    path("posts",views.posts, name= "posts-page"),
+    path("posts/<slug:slug>",views.post_detail
+         ,name ="post-detail-page"), # post/my-first-post
+    
+    
     path("contact",views.contact,name = "contact-page"),
     path("about",views.about,name = "about-page"),
     path("blog",views.blog,name = "blog-page"),
-    path("posts",views.posts, name= "posts-page"),
-    path("posts/<slug>",views.post_detail,name ="post-detail-page")
+    
 ]
