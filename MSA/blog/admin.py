2e213651a84cc1de6d52834e1dc1d90609997624
Mohamed Salem Ali <65522205+Mohamed-Salem-Ali@users.noreply.github.com>
2024-08-02 22:37:33 +0300

    Versions/v1 (#8)
    * Home page style
    
    * Adding all posts page
    
    * modified base.html & app.css
    
    * Squashed commit of the following:
    
    commit d376792ee81099424c79b87f0b309656280a712a
    Author: Mohamed Salem Ali <65522205+Mohamed-Salem-Ali@users.noreply.github.com>
    Date:   Tue Jul 9 15:07:16 2024 +0300
    
        Versions/v1 (#2)
    
        * Home page style
    
        * Adding all posts page
    
    commit b301969b8bad0322a6ade6cbbb8fed4cf29c5b58
    Merge: 50a6368 c0dea31
    Author: Mohamed Salem Ali <65522205+Mohamed-Salem-Ali@users.noreply.github.com>
    Date:   Sat Jul 6 22:03:01 2024 +0300
    
        Merge pull request #1 from Mohamed-Salem-Ali/versions/v1
    
        version 1
    
    * .
    
    * post details
    
    * changes and updates
    
    add details about each post
    add images to each post
    navigate between posts
    
    * Refactor blog project to use SQLite database and ORM for post data
    
    - Replaced in-memory list of dictionaries with SQLite database.
    - Created `Post` model with fields: slug, image, author, date, title, excerpt, and content.
    - Generated and applied migrations for the new model.
    - Populated database with initial post data.
    - Updated views to fetch posts from the database instead of the list.
    - Modified `index.html` and `post_detail.html` templates to display data from the database.
    - Implemented logic to display the latest 3 posts on the index page.
    - Added model representation for better display in the Django admin.
    
    * Update admin.py

--- MSA/blog/admin.py
+++ MSA/blog/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Post
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'date')
+    search_fields = ('title', 'author', 'content')
+    prepopulated_fields = {'slug': ('title',)}
+    list_filter = ('author', 'date')
+    ordering = ('-date',)
+
+admin.site.register(Post, PostAdmin)
