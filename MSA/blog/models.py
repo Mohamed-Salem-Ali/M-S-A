517e18a80169bc9e129fa82536cac3d0ef905b79
Mohamed-Salem-Ali <65522205+Mohamed-Salem-Ali@users.noreply.github.com>
2024-08-12 18:13:13 +0300

    Updates
    Add Slug to the URL

--- MSA/blog/models.py
+++ MSA/blog/models.py
@@ -1,13 +1,24 @@
 from django.db import models
-
+from django.utils.text import slugify
+from django.urls import reverse
 class Post(models.Model):
-    slug = models.SlugField(unique=True)
+    slug = models.SlugField(unique=True, default="",null=False,db_index=True)
     image = models.CharField(max_length=255)
     author = models.CharField(max_length=100)
     date = models.DateField()
     title = models.CharField(max_length=200)
     excerpt = models.TextField()
     content = models.TextField()
+    
+    
 
     def __str__(self):
         return self.title
+    
+    def get_absolute_url(self):
+        return reverse("post-detail-page", args=[self.pk])
+    
+    
+    def save(self,*args, **kwargs):
+        self.slug=slugify(self.title)
+        super().save(*args, **kwargs)
